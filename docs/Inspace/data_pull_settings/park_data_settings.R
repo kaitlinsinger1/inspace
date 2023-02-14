db2858d59bc378234e1d2efaff4537056709b5e0
aybloom <93952155+aybloom@users.noreply.github.com>
2023-02-14 08:49:02 -0800

    Add files via upload

--- docs/Inspace/data_pull_settings/park_data_settings.R
+++ docs/Inspace/data_pull_settings/park_data_settings.R
@@ -8,7 +8,10 @@ first_var<-parks_vars[1]
 parks_years<-c(2021)
 parks_selected_years<-c(2021)
 
-parks_description<-'The ParkServe dataset contains geographical information of parks in the US. Data is provided for download in a variety of file formats and configuration.'
+parks_description<-'The ParkServe dataset contains geographical information of parks in the US. 
+We will download files and use to calculate distance to the nearest park (within the set buffer), and the percent of land in a given buffer that is park land. You can learn more at https://www.tpl.org/parkserve.
+The first time you run this data pull, the file will be downloaded and processed in the background, which can take around 20 minutes. Note that if your firewall prevents you from downloading files, you may need to contact
+Amy and have the file sent directly to you.'
 
 download_file_park <- function () {  # download the external dataset and give it a name (will use it in creating external_data_name_to_info_list)
   download.file(url = "https://parkserve.tpl.org/downloads/ParkServe_Shapefiles_05042022.zip?_ga=2.103216521.887440371.1664905337-1364699585.1664905337", destfile = "external_data/ParkServe_shp.zip")
